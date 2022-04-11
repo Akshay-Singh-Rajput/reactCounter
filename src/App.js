1a4244116378208313a10a41c4659c9f140486e6
Akshay Kumar <97354310+Akshay-Singh-Rajput@users.noreply.github.com>
2022-04-11 08:32:04 +0530

    vercel trying

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+function CounterFun () {
+
+
+    const [ counter, setCounter ] = useState(0);
+    const handleAdd = () => {
+        if (counter >= 100){
+            return alert("Can't go above 100") ;
+        }
+        setCounter(counter + 1);
+    };
+    const handleSub = () => {
+        if (counter <= 0){
+            return alert("Can't go below 0") ;
+        }
+        setCounter(counter - 1);
+    };
+    const handleDouble = () => {
+        if (counter >= 100){
+            return alert("You have cross Maximum limit, Max is 100") ;
+        }
+       
+        setCounter(counter * 2);
+
+    };
+
+     return ( <div className='App'>
+        <h3 className={ counter % 2 === 0 ? "green"  : "red" }>Counter : { counter } </h3>
+        <button onClick={ handleAdd }>Add 1</button>
+        <button onClick={ handleSub }>Sub 1</button>
+        <button onClick={ handleDouble }>Double</button>
+        <div >Number is { counter % 2 === 0 ? "Even" : "Odd" }  </div>
+    </div>
+     );
+
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   return CounterFun();
   );
 }
 
